f364d897a8820075a96e5f7c702d04a1d2ece662
MOHD SHAIQ <md.shaiq2102@gmail.com>
2026-08-02 00:44:23 +0530

    Initial commit: app + Dockerfile

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.routr("/")
+def home():
+    return "Hello from my CI/CD pipeline!  Build:v1"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
